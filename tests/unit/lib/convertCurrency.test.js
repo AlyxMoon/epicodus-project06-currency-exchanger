485fc1fb1f4efb4e2f23750618e04374cdbe361a
AlyxMoon <allisterkmoon@gmail.com>
2021-07-02 11:57:12 -0700

    add test: convertCurrency | from USD to other

--- tests/unit/lib/convertCurrency.test.js
+++ tests/unit/lib/convertCurrency.test.js
@@ -1,6 +1,13 @@
 import convertCurrency from '@/lib/convertCurrency'
 
 describe('function convertCurrency()', () => {
+  const exchangeRates = {
+    USD: 1,
+    EUR: 0.8416,
+    MXN: 19.9907,
+    KES: 107.7367,
+  }
+
   it('should return 0 if amount is 0 or not provided', () => {
     expect(convertCurrency()).toEqual(0)
     expect(convertCurrency({ amount: 0 })).toEqual(0)
@@ -9,8 +16,8 @@ describe('function convertCurrency()', () => {
   it('should throw an error if baseCurrency or targetCurrency is not in the exchangeRates arg', () => {
     expect(() => {
       convertCurrency({
-        exchangeRates: { USD: 1, MXN: 1 },
-        baseCurrency: 'EUR',
+        exchangeRates,
+        baseCurrency: 'HKD',
         targetCurrency: 'MXN',
         amount: 1,
       })
@@ -18,20 +25,36 @@ describe('function convertCurrency()', () => {
 
     expect(() => {
       convertCurrency({
-        exchangeRates: { USD: 1, MXN: 1 },
+        exchangeRates,
         baseCurrency: 'USD',
-        targetCurrency: 'EUR',
+        targetCurrency: 'JOD',
         amount: 1,
       })
     }).toThrow() // target currency not in it
 
     expect(() => {
       convertCurrency({
-        exchangeRates: { USD: 1, MXN: 1 },
-        baseCurrency: 'EUR',
+        exchangeRates,
+        baseCurrency: 'GIP',
         targetCurrency: 'RUB',
         amount: 1,
       })
     }).toThrow() // neither currency in it
+  })
+
+  it('should correctly convert a currency from USD', () => {
+    expect(convertCurrency({
+      exchangeRates,
+      baseCurrency: 'USD',
+      targetCurrency: 'EUR',
+      amount: 10,
+    })).toBeCloseTo(8.416, 4)
+
+    expect(convertCurrency({
+      exchangeRates,
+      baseCurrency: 'USD',
+      targetCurrency: 'KES',
+      amount: 10,
+    })).toBeCloseTo(1077.3670, 4)
   })
 })
