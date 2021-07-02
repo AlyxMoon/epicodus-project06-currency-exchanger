7a892c70a464559d7dd10c6229f8f36fc56135f8
AlyxMoon <allisterkmoon@gmail.com>
2021-07-02 14:39:42 -0700

    add some additional api tests for error handling

--- tests/unit/api/exchangeRate.test.js
+++ tests/unit/api/exchangeRate.test.js
@@ -2,6 +2,7 @@ import ExchangeRateApi from '@/api/exchangeRate'
 
 describe('class ExchangeRateApi', () => {
   global.fetch = jest.fn(() => Promise.resolve({
+    ok: true,
     json: () => Promise.resolve({ result: 'success' }),
   }))
 
@@ -22,5 +23,23 @@ describe('class ExchangeRateApi', () => {
     expect(fetch).toHaveBeenCalledTimes(1)
     expect(fetch).toHaveBeenCalledWith('https://v6.exchangerate-api.com/v6/42/latest/USD')
     expect(result).toEqual({ result: 'success' })
+  })
+
+  it('getApiData(): should throw an error if the server did not give an ok response', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      ok: false,
+      json: () => Promise.resolve({ result: 'error' }),
+    }))
+
+    await expect(ExchangeRateApi.getApiData()).rejects.toBeTruthy()
+  })
+
+  it('getApiData(): should throw an error if the server responded with an error result', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({ result: 'error' }),
+    }))
+
+    await expect(ExchangeRateApi.getApiData()).rejects.toBeTruthy()
   })
 })
