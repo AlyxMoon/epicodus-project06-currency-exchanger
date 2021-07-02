6e1fc9b850b5be1e42273389ed0d57491137d696
AlyxMoon <allisterkmoon@gmail.com>
2021-07-02 11:48:14 -0700

    add tests for checking currencies in the exchangeRate

--- tests/unit/lib/convertCurrency.test.js
+++ tests/unit/lib/convertCurrency.test.js
@@ -5,4 +5,33 @@ describe('function convertCurrency()', () => {
     expect(convertCurrency()).toEqual(0)
     expect(convertCurrency({ amount: 0 })).toEqual(0)
   })
+
+  it('should throw an error if baseCurrency or targetCurrency is not in the exchangeRates arg', () => {
+    expect(() => {
+      convertCurrency({
+        exchangeRates: { USD: 1, MXN: 1 },
+        baseCurrency: 'EUR',
+        targetCurrency: 'MXN',
+        amount: 1,
+      })
+    }).toThrow() // base currency not in it
+
+    expect(() => {
+      convertCurrency({
+        exchangeRates: { USD: 1, MXN: 1 },
+        baseCurrency: 'USD',
+        targetCurrency: 'EUR',
+        amount: 1,
+      })
+    }).toThrow() // target currency not in it
+
+    expect(() => {
+      convertCurrency({
+        exchangeRates: { USD: 1, MXN: 1 },
+        baseCurrency: 'EUR',
+        targetCurrency: 'RUB',
+        amount: 1,
+      })
+    }).toThrow() // neither currency in it
+  })
 })
