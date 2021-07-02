6c266bf024d34f5c1ebf9013f50785a0cbfc49d4
AlyxMoon <allisterkmoon@gmail.com>
2021-07-02 10:44:45 -0700

    update tests to fully check for cache retrieval

--- src/api/cache.js
+++ src/api/cache.js
@@ -3,8 +3,6 @@ export default class Cache {
   constructor (cacheInterface = localStorage, key = 'cache') {
     this.interface = cacheInterface
     this.key = key
-
-    this.loadCache()
   }
 
   get data () {
--- tests/unit/cache.test.js
+++ tests/unit/cache.test.js
@@ -1,6 +1,16 @@
 import Cache from '@/api/cache'
 
 describe('class Cache', () => {
+  it('should load default values when no arguments provided', () => {
+    global.localStorage = {
+      getItem: jest.fn(() => {}),
+    }
+
+    const cache = new Cache()
+    expect(cache.interface).toEqual(localStorage)
+    expect(cache.key).toEqual('cache')
+  })
+
   it('should begin with an empty object when there is no current cache', () => {
     const cacheInterface = {
       getItem: jest.fn(() => null),
@@ -8,9 +18,9 @@ describe('class Cache', () => {
 
     const cache = new Cache(cacheInterface, 'test-cache')
 
+    expect(cache.data).toEqual({})
     expect(cacheInterface.getItem).toHaveBeenCalledTimes(1)
     expect(cacheInterface.getItem).toHaveBeenCalledWith('test-cache')
-    expect(cache.data).toEqual({})
   })
 
   it('should load data from cache when it exists', () => {
@@ -20,9 +30,9 @@ describe('class Cache', () => {
 
     const cache = new Cache(cacheInterface, 'test-cache')
 
+    expect(cache.data).toEqual({ item1: 1, item2: 2 })
     expect(cacheInterface.getItem).toHaveBeenCalledTimes(1)
     expect(cacheInterface.getItem).toHaveBeenCalledWith('test-cache')
-    expect(cache.data).toEqual({ item1: 1, item2: 2 })
   })
 
   it('should load data from correct cache key, if multiple items exist', () => {
@@ -37,8 +47,21 @@ describe('class Cache', () => {
 
     const cache = new Cache(cacheInterface, 'cache2')
 
+    expect(cache.data).toEqual({ item2: 2 })
     expect(cacheInterface.getItem).toHaveBeenCalledTimes(1)
     expect(cacheInterface.getItem).toHaveBeenCalledWith('cache2')
-    expect(cache.data).toEqual({ item2: 2 })
+  })
+
+  it('should load same data without pulling from storage again on subsequent calls', () => {
+    const cacheInterface = {
+      getItem: jest.fn(() => ({ item1: 1, item2: 2 })),
+    }
+
+    const cache = new Cache(cacheInterface)
+
+    expect(cache.data).toEqual({ item1: 1, item2: 2 })
+    expect(cacheInterface.getItem).toHaveBeenCalledTimes(1)
+    expect(cache.data).toEqual({ item1: 1, item2: 2 })
+    expect(cacheInterface.getItem).toHaveBeenCalledTimes(1)
   })
 })
